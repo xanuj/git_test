572492ae122f6255890d980a1ce762279f83c9b1
Anuj <128241585+xanuj@users.noreply.github.com.>
2025-08-22 19:58:52 +0530

    adding test.js and index.html

---
+++ test.js
@@ -0,0 +1,6 @@
+let email=String("anujsaini121314@gmail.com");
+console.log(typeof(email));
+let url= new String("https://stackoverflow.com/questions/66168985/how-to-change-folder-that-opened-by-default-in-vscode")
+console.log(url.replaceAll('-','%20'))
+const part=url.split("/")
+console.log(part)
