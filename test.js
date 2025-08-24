94cf8eb9f94a78ba469eb8cd9bc0e188f82e616e
Anuj <128241585+xanuj@users.noreply.github.com.>
2025-08-24 19:06:58 +0530

    updating test.js

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
+const mysym=Symbol("key1")
+const JsUser={
+    name: "Anuj",
+    age: 21,
+    "my name" :"Anuj saini",
+    email: "anujsaini121314@gmail.com",
+    [mysym] :"mykey1",
+    greeting : function(){
+        console.log(`welcome ${this["my name"]}`)
+    }
+}
+
+const chai =function(){
+    console.log("hellow world")
+}
+const arrow_function = ()=>{
+    console.log("hellow world in arrow")
+}
+chai();
+arrow_function();
+
+
+//destructuring the objcet
+const {name:username}=JsUser
+console.log(username);
+
+//iife 
+(function chai(){
+    console.log("hello world")
+})();
+
+// loops
 
