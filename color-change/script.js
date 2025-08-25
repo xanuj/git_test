aebf0d22eb6abed7b6aa5c1c40268a92e914163b
Anuj <128241585+xanuj@users.noreply.github.com.>
2025-08-25 23:44:19 +0530

    background color changer

---
+++ color-change/script.js
@@ -0,0 +1,10 @@
+const buttons=document.querySelectorAll(".button")
+const body=document.querySelector("body")
+
+buttons.forEach((button)=>{
+    console.log(button)
+    button.addEventListener('click',function(e){
+        console.log(e.target)
+            body.style.backgroundColor=e.target.id;
+    })
+})
